cd4e8277b4563072ba61e194e38e28a98bd9b620
christian-nieves <chrisnieves81@gmail.com>
2018-05-19 16:10:32 -0400

    Added bootstrap and a search field

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-row class="justify-content-md-center">
+    <b-col cols="6">
+      <b-form-input
+        type="text"
+        placeholder="Search for a #"
+      />
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+
+};
+</script>
+
+<style scoped>
+
+</style>
